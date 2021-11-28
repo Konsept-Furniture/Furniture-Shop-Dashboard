b40ca20e9c5b99c81d7b877a1ec3954ad12f7936
Vo Hoang Duc Khoa <19520646@gm.uit.edu.vn>
2021-11-28 11:46:03 +0700

    add use-auth hook to control auth

---
+++ src/hooks/use-auth.ts
@@ -0,0 +1,41 @@
+import { LoginPayload } from './../models'
+import { authApi } from './../api-client'
+import useSWR from 'swr'
+import { PublicConfiguration } from 'swr/dist/types'
+import axiosClient from 'api-client/axios-client'
+
+const fetcher = (url: string) => {
+   axiosClient.get(url).then(res => {
+      console.log(res.data)
+      return res.data
+   })
+}
+
+export function useAuth(options?: Partial<PublicConfiguration>) {
+   const { data, error, mutate } = useSWR('users/read/infor', {
+      dedupingInterval: 60 * 60 * 1000, // 1hr
+      revalidateOnFocus: false,
+      ...options,
+   })
+
+   const firstLoading = data === undefined && error === undefined
+
+   async function login(payload: LoginPayload) {
+      await authApi.login(payload)
+
+      await mutate()
+   }
+
+   async function logout() {
+      await authApi.logout()
+      mutate({}, false)
+   }
+
+   return {
+      data,
+      error,
+      login,
+      logout,
+      firstLoading,
+   }
+}
