a2aab9c65d56a6b44b6562371de2041931945c55
Vo Hoang Duc Khoa <19520646@gm.uit.edu.vn>
2021-11-28 11:44:49 +0700

    add models

---
+++ src/models/user.ts
@@ -0,0 +1,12 @@
+export interface User {
+   _id: string
+   username: string
+   email: string
+   isAdmin: boolean
+   phone: string
+   address: string
+   name: string
+   deleted: boolean
+   createdAt: string
+   updatedAt: string
+}
