25e7d97a215a8e352bcbb090488fcfa35ee71d79
Vo Hoang Duc Khoa <19520646@gm.uit.edu.vn>
2021-11-28 12:17:54 +0700

    migrate 404, account, product, settings page to ts

---
+++ src/pages/customers.tsx
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+import { Box, Container } from '@mui/material'
+import { CustomerListResults } from '../components/customer/customer-list-results'
+import { CustomerListToolbar } from '../components/customer/customer-list-toolbar'
+import { customers } from '../__mocks__/customers'
+import { DashboardLayout } from 'components/layouts/dashboard-layout'
+
+const Customers = () => (
+   <>
+      <Head>
+         <title>Customers | Material Kit</title>
+      </Head>
+      <Box
+         component="main"
+         sx={{
+            flexGrow: 1,
+            py: 8,
+         }}
+      >
+         <Container maxWidth={false}>
+            <CustomerListToolbar />
+            <Box sx={{ mt: 3 }}>
+               <CustomerListResults customers={customers} />
+            </Box>
+         </Container>
+      </Box>
+   </>
+)
+Customers.Layout = DashboardLayout
+
+export default Customers
