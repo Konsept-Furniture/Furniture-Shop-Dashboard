0a9be46a6e46dc853a2d4e4529e4ca0b0fa1e70f
Vo Hoang Duc Khoa <19520646@gm.uit.edu.vn>
2021-11-28 11:43:57 +0700

    folderize dashboard layout

---
+++ src/components/layouts/dashboard-layout.js
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import { Box } from '@mui/material'
+import { styled } from '@mui/material/styles'
+import { DashboardNavbar } from './dashboard-navbar'
+import { DashboardSidebar } from './dashboard-sidebar'
+import { Auth } from '../auth'
+
+const DashboardLayoutRoot = styled('div')(({ theme }) => ({
+   display: 'flex',
+   flex: '1 1 auto',
+   maxWidth: '100%',
+   paddingTop: 64,
+   [theme.breakpoints.up('lg')]: {
+      paddingLeft: 280,
+   },
+}))
+
+export const DashboardLayout = props => {
+   const { children } = props
+   const [isSidebarOpen, setSidebarOpen] = useState(true)
+
+   return (
+      <Auth>
+         <DashboardLayoutRoot>
+            <Box
+               sx={{
+                  display: 'flex',
+                  flex: '1 1 auto',
+                  flexDirection: 'column',
+                  width: '100%',
+               }}
+            >
+               {children}
+            </Box>
+         </DashboardLayoutRoot>
+         <DashboardNavbar onSidebarOpen={() => setSidebarOpen(true)} />
+         <DashboardSidebar onClose={() => setSidebarOpen(false)} open={isSidebarOpen} />
+      </Auth>
+   )
+}
