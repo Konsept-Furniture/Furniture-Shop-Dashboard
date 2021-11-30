d379337cbcae0b17ee350bac937bb0e4de6e9790
Vo Hoang Duc Khoa <19520646@gm.uit.edu.vn>
2021-11-30 23:23:23 +0700

    feat: add logout api

---
+++ src/pages/api/auth/logout.ts
@@ -0,0 +1,28 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import httpProxy, { ProxyResCallback } from 'http-proxy'
+import { ResponseData, User } from '../../../models'
+import Cookies from 'cookies'
+
+export const config = {
+   api: {
+      bodyParser: false,
+   },
+}
+export default function handler(req: NextApiRequest, res: NextApiResponse<ResponseData<string>>) {
+   if (req.method !== 'POST') {
+      return res.status(404).json({
+         data: null,
+         errorCode: 0,
+         message: 'Method is not supported',
+      })
+   }
+
+   const cookies = new Cookies(req, res)
+   cookies.set('access_token')
+
+   res.status(200).json({
+      data: null,
+      errorCode: 0,
+      message: 'logout successfully',
+   })
+}
