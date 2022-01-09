758cd545e25fcd988ed51784dbeb4d978864ace7
Vo Hoang Duc Khoa <19520646@gm.uit.edu.vn>
2022-01-09 22:16:38 +0700

    test

---
+++ src/components/customer/customer-basic-info-card.tsx
@@ -0,0 +1,233 @@
+import {
+   Card,
+   CardContent,
+   CardHeader,
+   Divider,
+   List,
+   ListItem,
+   Skeleton,
+   Typography
+} from '@mui/material'
+import { Box } from '@mui/system'
+import axios, { AxiosResponse } from 'axios'
+import { format, parseISO } from 'date-fns'
+import { District, Province, User, Ward } from 'models'
+import React from 'react'
+import useSWR from 'swr'
+
+export interface CustomerBasicInfoCardProps {
+   customer?: User
+}
+
+function fetcher<T>(url: string) {
+   return axios.get<any, AxiosResponse<T>>(url).then((res: AxiosResponse<T>): T => {
+      return res.data
+   })
+}
+
+export function CustomerBasicInfoCard({ customer }: CustomerBasicInfoCardProps) {
+   const { data: customerProvince } = useSWR<Province>(
+      () =>
+         customer && customer?.deliveryInfo.address.province
+            ? `https://provinces.open-api.vn/api/p/${customer?.deliveryInfo.address.province}`
+            : null,
+      fetcher,
+      {
+         revalidateOnFocus: false
+      }
+   )
+   const { data: customerDistrict } = useSWR<District>(
+      () =>
+         customer && customer?.deliveryInfo.address.province
+            ? `https://provinces.open-api.vn/api/d/${customer?.deliveryInfo.address.district}`
+            : null,
+      fetcher,
+      {
+         revalidateOnFocus: false
+      }
+   )
+   const { data: customerWard } = useSWR<Ward>(
+      () =>
+         customer && customer?.deliveryInfo.address.province
+            ? `https://provinces.open-api.vn/api/w/${customer?.deliveryInfo.address.ward}`
+            : null,
+      fetcher,
+      {
+         revalidateOnFocus: false
+      }
+   )
+
+   return (
+      <Card>
+         <CardHeader title="Basic details" />
+         <Divider />
+         <CardContent sx={{ p: 0 }}>
+            {customer ? (
+               <List>
+                  <ListItem
+                     sx={{ px: 3, py: 1.5, display: 'flex', flexDirection: 'row', my: 0 }}
+                     alignItems="center"
+                     disablePadding
+                  >
+                     <Typography variant="subtitle2" sx={{ minWidth: 180 }}>
+                        Full Name
+                     </Typography>
+                     <Box sx={{ flex: 1 }}>
+                        <Typography variant="body2" color="text.secondary">
+                           {customer.name}
+                        </Typography>
+                     </Box>
+                  </ListItem>
+                  <Divider />
+
+                  <ListItem
+                     sx={{ px: 3, py: 1.5, display: 'flex', flexDirection: 'row', my: 0 }}
+                     alignItems="center"
+                     disablePadding
+                  >
+                     <Typography variant="subtitle2" sx={{ minWidth: 180 }}>
+                        Username
+                     </Typography>
+                     <Box sx={{ flex: 1 }}>
+                        <Typography variant="body2" color="text.secondary">
+                           {customer.username}
+                        </Typography>
+                     </Box>
+                  </ListItem>
+                  <Divider />
+
+                  <ListItem
+                     sx={{ px: 3, py: 1.5, display: 'flex', flexDirection: 'row', my: 0 }}
+                     alignItems="center"
+                     disablePadding
+                  >
+                     <Typography variant="subtitle2" sx={{ minWidth: 180 }}>
+                        Phone Number
+                     </Typography>
+                     <Box sx={{ flex: 1 }}>
+                        <Typography variant="body2" color="text.secondary">
+                           {customer.phone}
+                        </Typography>
+                     </Box>
+                  </ListItem>
+                  <Divider />
+
+                  <ListItem
+                     sx={{ px: 3, py: 1.5, display: 'flex', flexDirection: 'row', my: 0 }}
+                     alignItems="center"
+                     disablePadding
+                  >
+                     <Typography variant="subtitle2" sx={{ minWidth: 180 }}>
+                        Email
+                     </Typography>
+                     <Box sx={{ flex: 1 }}>
+                        <Typography variant="body2" color="text.secondary">
+                           {customer.email}
+                        </Typography>
+                     </Box>
+                  </ListItem>
+                  <Divider />
+
+                  <ListItem
+                     sx={{ px: 3, py: 1.5, display: 'flex', flexDirection: 'row', my: 0 }}
+                     alignItems="center"
+                     disablePadding
+                  >
+                     <Typography variant="subtitle2" sx={{ minWidth: 180 }}>
+                        Delivery Information
+                     </Typography>
+                     <Box sx={{ flex: 1 }}>
+                        <Typography variant="body2" color="text.secondary">
+                           {customer.deliveryInfo.address.street}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                           {customerProvince?.name}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                           {customerDistrict?.name}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                           {customerWard?.name}
+                        </Typography>
+                     </Box>
+                  </ListItem>
+                  <Divider />
+
+                  <ListItem
+                     sx={{ px: 3, py: 1.5, display: 'flex', flexDirection: 'row', my: 0 }}
+                     alignItems="center"
+                     disablePadding
+                  >
+                     <Typography variant="subtitle2" sx={{ minWidth: 180 }}>
+                        Registration Date
+                     </Typography>
+                     <Box sx={{ flex: 1 }}>
+                        <Typography variant="body2" color="text.secondary">
+                           {customer.createdAt &&
+                              format(parseISO(customer.createdAt), 'dd/MM/yyyy HH:mm')}
+                        </Typography>
+                     </Box>
+                  </ListItem>
+                  <Divider />
+
+                  <ListItem
+                     sx={{ px: 3, py: 1.5, display: 'flex', flexDirection: 'row', my: 0 }}
+                     alignItems="center"
+                     disablePadding
+                  >
+                     <Typography variant="subtitle2" sx={{ minWidth: 180 }}>
+                        Payment Method
+                     </Typography>
+                     <Box sx={{ flex: 1 }}>
+                        <Typography variant="body2" color="text">
+                           {/* {customer.payment} */}
+                        </Typography>
+                     </Box>
+                  </ListItem>
+                  <Divider />
+
+                  <ListItem
+                     sx={{ px: 3, py: 1.5, display: 'flex', flexDirection: 'row', my: 0 }}
+                     alignItems="center"
+                     disablePadding
+                  >
+                     <Typography variant="subtitle2" sx={{ minWidth: 180 }}>
+                        Total Amount
+                     </Typography>
+                     <Box sx={{ flex: 1 }}>
+                        <Typography variant="body2" color="text">
+                           {/* ${customer.amount.toFixed(2)} */}
+                        </Typography>
+                     </Box>
+                  </ListItem>
+                  <Divider />
+
+                  <ListItem
+                     sx={{ px: 3, pt: 1.5, display: 'flex', flexDirection: 'row', my: 0 }}
+                     alignItems="center"
+                     disablePadding
+                  >
+                     <Typography variant="subtitle2" sx={{ minWidth: 180 }}>
+                        Status
+                     </Typography>
+                     <Box sx={{ flex: 1 }}>
+                        <Typography variant="body2" color="text">
+                           {/* {customer.status} */}
+                        </Typography>
+                     </Box>
+                  </ListItem>
+               </List>
+            ) : (
+               <List>
+                  {Array.from(new Array(6)).map((i, idx) => (
+                     <ListItem key={idx} sx={{ px: 3, pt: 1.5 }} alignItems="center" disablePadding>
+                        <Skeleton variant="text" sx={{ flex: 1, mb: 1 }} height={40} />
+                        <Divider />
+                     </ListItem>
+                  ))}
+               </List>
+            )}
+         </CardContent>
+      </Card>
+   )
+}
---
+++ src/pages/customers/[customerId]/edit.tsx
@@ -0,0 +1,133 @@
+import ArrowBackIcon from '@mui/icons-material/ArrowBack'
+import { Avatar, Box, Button, Chip, Container, Grid, Skeleton, Typography } from '@mui/material'
+import { customerApi } from 'api-client'
+import axiosClient from 'api-client/axios-client'
+import { CustomerBasicInfoCardEdit } from 'components/customer'
+import { DashboardLayout } from 'components/layouts/dashboard-layout'
+import { ResponseData, User } from 'models'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import React from 'react'
+import useSWR from 'swr'
+import { getInitials } from 'utils'
+
+export interface EditCustomerPageProps {}
+
+const fetcher = (url: string) => {
+   return axiosClient.get<any, ResponseData<User>>(url).then((res: ResponseData<User>): User => {
+      return res.data
+   })
+}
+const EditCustomerPage = (props: EditCustomerPageProps) => {
+   const router = useRouter()
+   const { customerId } = router.query
+
+   const { data: customer, mutate } = useSWR(`users/${customerId}`, fetcher, {
+      revalidateOnFocus: false
+   })
+
+   const handleUpdateBasicInfo = async (payload: Partial<User>) => {
+      if (typeof customerId === 'string') {
+         try {
+            await customerApi.update(customerId, payload).then(res => {
+               console.log(res)
+               mutate(res.data, true)
+               router.push(`/customers/${customerId}`)
+            })
+         } catch (error) {
+            console.log('error to update customer', error)
+         }
+      }
+   }
+   const handleDeleteCustomer = async () => {
+      if (typeof customerId === 'string') {
+         try {
+            await customerApi.delete(customerId).then(res => {
+               console.log(res)
+               router.push('/customers')
+            })
+         } catch (error) {
+            console.log('error to delete customer', error)
+         }
+      }
+   }
+
+   return (
+      <>
+         <Box
+            component="main"
+            sx={{
+               flexGrow: 1,
+               pt: 6,
+               pb: 12,
+               px: 6
+            }}
+         >
+            <Container maxWidth={false}>
+               <Box
+                  sx={{
+                     alignItems: 'center',
+                     display: 'flex',
+                     justifyContent: 'space-between',
+                     flexWrap: 'wrap'
+                  }}
+               >
+                  <Link href="/customers" passHref>
+                     <Button variant="text" startIcon={<ArrowBackIcon />}>
+                        Customers
+                     </Button>
+                  </Link>
+               </Box>
+               <Grid
+                  container
+                  sx={{
+                     mt: 1,
+                     alignItems: 'center',
+                     justifyContent: 'space-between'
+                  }}
+               >
+                  {customer ? (
+                     <Grid item sx={{ m: 1 }}>
+                        <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+                           <Avatar sx={{ width: 56, height: 56 }} src="">
+                              {getInitials(customer.name)}
+                           </Avatar>
+                           <Box sx={{ flex: 1 }}>
+                              <Typography variant="h4">{customer.email}</Typography>
+                              <Typography
+                                 variant="subtitle2"
+                                 sx={{ display: 'flex', alignItems: 'center', gap: 1 }}
+                              >
+                                 user_id: <Chip size="small" label={customer._id} />
+                              </Typography>
+                           </Box>
+                        </Box>
+                     </Grid>
+                  ) : (
+                     <Grid item>
+                        <Typography sx={{ m: 1 }} variant="h4">
+                           <Skeleton variant="text" width="300px" />
+                        </Typography>
+                        <Typography sx={{ m: 1 }} variant="body2" color="textSecondary">
+                           <Skeleton variant="text" width="300px" />
+                        </Typography>
+                     </Grid>
+                  )}
+               </Grid>
+
+               <Box sx={{ ml: 1, mt: 4 }}>
+                  <CustomerBasicInfoCardEdit
+                     customer={customer}
+                     onSave={handleUpdateBasicInfo}
+                     onDelete={handleDeleteCustomer}
+                  />
+               </Box>
+            </Container>
+         </Box>
+      </>
+   )
+}
+
+EditCustomerPage.Layout = DashboardLayout
+
+export default EditCustomerPage
