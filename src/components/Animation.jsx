d69f842c4cfa137cf6e0034f1ffa9de625b49b3e
Nhien Nguyen <53686305+annhienktuit@users.noreply.github.com>
2022-01-12 11:26:19 +0700

    feat: add loading animation

---
+++ src/components/Animation.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactLoading from 'react-loading';
+
+const Animation = ({ type, color }) => (
+	<ReactLoading type={"bubbles"} color={"#ef5350"} height={'10%'} width={'10%'} />
+);
+
+export default Animation;
