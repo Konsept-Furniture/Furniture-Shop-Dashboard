95d26b0522db1ff93769dba7ece84b8c5705b713
Nhien Nguyen <53686305+annhienktuit@users.noreply.github.com>
2022-01-12 10:43:14 +0700

    fix rounded number to 1 digit

--- src/components/dashboard/total-customers.tsx
+++ src/components/dashboard/total-customers.tsx
@@ -54,7 +54,7 @@ export const TotalCustomers = ({ data, ...restProps }: { data?: CardData }) => (
                      mr: 1
                   }}
                >
-                  {Math.abs(data.compareLastMonth).toFixed(2)}%
+                  {Math.abs(data.compareLastMonth).toFixed(1)}%
                </Typography>
                <Typography color="textSecondary" variant="caption">
                   Since last month
--- src/components/dashboard/total-orders.tsx
+++ src/components/dashboard/total-orders.tsx
@@ -64,7 +64,7 @@ export const TotalOrders = ({ data, ...restProps }: { data?: CardData }) => (
                      mr: 1
                   }}
                >
-                  {Math.abs(data.compareLastMonth).toFixed(2)}%
+                  {Math.abs(data.compareLastMonth).toFixed(1)}%
                </Typography>
                <Typography color="textSecondary" variant="caption">
                   Since last month
